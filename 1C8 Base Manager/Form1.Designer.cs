5a87ae9cc4b4cb0ee8441f56119168b6b2894156
Eugene <Eugene@LINUX>
2024-12-10 17:53:25 +0200

    Add project files.

---
+++ 1C8 Base Manager/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace _1C8_Base_Manager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 32);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(475, 674);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(547, 32);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(475, 674);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(366, 5);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(547, 5);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Gray;
+            this.button1.Location = new System.Drawing.Point(503, 310);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(29, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "▶️";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Gray;
+            this.button2.Location = new System.Drawing.Point(503, 366);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(29, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "◀️";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(865, 709);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(992, 13);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(30, 13);
+            this.linkLabel1.TabIndex = 7;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "(ง︡\'-\'︠)ง";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(1034, 723);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "1C8 BM";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ 1C8 Base Manager/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace _1C8_Base_Manager
+{
+    public partial class Form1 : Form
+    {
+        private string currentUserPath1;
+        private string currentUserPath2;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeForm();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        private void InitializeForm()
+        {
+            // Setup DataGridView
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.MultiSelect = true;
+            dataGridView2.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView2.MultiSelect = true;
+
+            // Setup ComboBoxes with users
+            PopulateUserList();
+
+            // Event Handlers
+            comboBox1.SelectedIndexChanged += ComboBox1_SelectedIndexChanged;
+            comboBox2.SelectedIndexChanged += ComboBox2_SelectedIndexChanged;
+            button1.Click += BtnCopyRight_Click;
+            button2.Click += BtnCopyLeft_Click;
+        }
+
+        private void PopulateUserList()
+        {
+            string usersDirectory = @"C:\Users";
+            string[] userDirectories = Directory.GetDirectories(usersDirectory);
+
+            comboBox1.Items.Clear();
+            comboBox2.Items.Clear();
+
+            foreach (string userDir in userDirectories)
+            {
+                string userName = new DirectoryInfo(userDir).Name;
+                comboBox1.Items.Add(userName);
+                comboBox2.Items.Add(userName);
+            }
+
+            label1.Text = "Data loaded";
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedUser = comboBox1.SelectedItem.ToString();
+            currentUserPath1 = $@"C:\Users\{selectedUser}\AppData\Roaming\1C\1CEStart\ibases.v8i";
+            LoadConnectionsFromFile(currentUserPath1, dataGridView1);
+        }
+
+        private void ComboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedUser = comboBox2.SelectedItem.ToString();
+            currentUserPath2 = $@"C:\Users\{selectedUser}\AppData\Roaming\1C\1CEStart\ibases.v8i";
+            LoadConnectionsFromFile(currentUserPath2, dataGridView2);
+        }
+
+        private void LoadConnectionsFromFile(string filePath, DataGridView gridView)
+        {
+            gridView.Rows.Clear();
+            gridView.Columns.Clear();
+
+            // Add columns
+            gridView.Columns.Add("Name", "Name");
+            gridView.Columns.Add("Server", "Server");
+            gridView.Columns.Add("Reference", "Reference");
+            gridView.Columns.Add("ID", "ID");
+
+            if (!File.Exists(filePath))
+            {
+                label1.Text = "File not found.";
+                return;
+            }
+
+            string fileContent = File.ReadAllText(filePath);
+            MatchCollection matches = Regex.Matches(fileContent, @"\[([^\]]+)\]([^[]+)(?=\[|$)", RegexOptions.Singleline);
+
+            foreach (Match match in matches)
+            {
+                string name = match.Groups[1].Value.Trim();
+                string details = match.Groups[2].Value;
+
+                string server = ExtractValue(details, @"Srvr=""([^""]+)""");
+                string reference = ExtractValue(details, @"Ref=""([^""]+)""");
+                string id = ExtractValue(details, @"ID=([^\r\n]+)");
+
+                gridView.Rows.Add(name, server, reference, id);
+            }
+        }
+
+        private string ExtractValue(string text, string pattern)
+        {
+            Match match = Regex.Match(text, pattern);
+            return match.Success ? match.Groups[1].Value : string.Empty;
+        }
+
+        private void BtnCopyRight_Click(object sender, EventArgs e)
+        {
+            CopySelectedRows(dataGridView1, dataGridView2, currentUserPath2);
+        }
+
+        private void BtnCopyLeft_Click(object sender, EventArgs e)
+        {
+            CopySelectedRows(dataGridView2, dataGridView1, currentUserPath1);
+        }
+        private void CopySelectedRows(DataGridView sourceGrid, DataGridView destGrid, string destFilePath)
+        {
+            if (sourceGrid.SelectedRows.Count == 0) return;
+
+            try
+            {
+                using (StreamWriter writer = File.AppendText(destFilePath))
+                {
+                    foreach (DataGridViewRow row in sourceGrid.SelectedRows)
+                    {
+                        string connectionBlock = GenerateConnectionBlock(row);
+                        writer.WriteLine(connectionBlock);
+                    }
+                }
+
+                // Reload the destination file
+                LoadConnectionsFromFile(destFilePath, destGrid);
+                label1.Text = "Connections copied successfully!" ;
+            }
+            catch (Exception ex)
+            {
+                label1.Text = $"Error copying connections: {ex.Message}";
+            }
+        }
+
+        private string GenerateConnectionBlock(DataGridViewRow row)
+        {
+            string name = row.Cells[0].Value?.ToString() ?? "";
+            string server = row.Cells[1].Value?.ToString() ?? "";
+            string reference = row.Cells[2].Value?.ToString() ?? "";
+            string id = row.Cells[3].Value?.ToString() ?? "";
+
+            return $@"[{name}]
+Connect=Srvr=""{server}"";Ref=""{reference}"";
+ID={id}
+OrderInList=16384
+Folder=/
+OrderInTree=256
+External=0
+ClientConnectionSpeed=Normal
+App=Auto
+WA=1
+Version=8.3";
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            string url = "https://github.com/e-gaydarzhi";
+            Process.Start(url);
+        }
+    }
+}
