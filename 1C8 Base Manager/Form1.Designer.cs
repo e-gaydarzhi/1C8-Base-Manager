9fc1effa56687fe30032e09660aceacb4648d81e
Eugene <Eugene@LINUX>
2024-12-10 19:44:55 +0200

    Bug Fix

--- 1C8 Base Manager/Form1.Designer.cs
+++ 1C8 Base Manager/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
@@ -36,6 +37,8 @@
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
@@ -64,6 +67,7 @@
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 2;
+            this.comboBox1.Text = "Select User";
             // 
             // comboBox2
             // 
@@ -73,11 +77,12 @@
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(121, 21);
             this.comboBox2.TabIndex = 3;
+            this.comboBox2.Text = "Select User";
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.Color.Gray;
-            this.button1.Location = new System.Drawing.Point(503, 310);
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(502, 277);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(29, 23);
             this.button1.TabIndex = 4;
@@ -86,8 +91,8 @@
             // 
             // button2
             // 
-            this.button2.BackColor = System.Drawing.Color.Gray;
-            this.button2.Location = new System.Drawing.Point(503, 366);
+            this.button2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button2.Location = new System.Drawing.Point(502, 306);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(29, 23);
             this.button2.TabIndex = 5;
@@ -98,7 +103,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(865, 709);
+            this.label1.Location = new System.Drawing.Point(861, 709);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 6;
@@ -115,12 +120,35 @@
             this.linkLabel1.Text = "(ง︡\'-\'︠)ง";
             this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button3.Location = new System.Drawing.Point(503, 335);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(28, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "🗑️";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button4.Location = new System.Drawing.Point(503, 364);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(28, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "💾";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Gray;
+            this.BackColor = System.Drawing.Color.LightGray;
             this.ClientSize = new System.Drawing.Size(1034, 723);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
@@ -129,6 +157,7 @@
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.dataGridView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "1C8 BM";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -149,6 +178,8 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- 1C8 Base Manager/Form1.cs
+++ 1C8 Base Manager/Form1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
@@ -8,9 +9,11 @@ namespace _1C8_Base_Manager
 {
     public partial class Form1 : Form
     {
+        //
         private string currentUserPath1;
         private string currentUserPath2;
-
+        private DataGridView lastActiveGridView;
+        
         public Form1()
         {
             InitializeComponent();
@@ -20,24 +23,27 @@ namespace _1C8_Base_Manager
         private void Form1_Load(object sender, EventArgs e)
         {
             dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView2.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
         }
 
         private void InitializeForm()
         {
-            // Setup DataGridView
             dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
             dataGridView1.MultiSelect = true;
             dataGridView2.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
             dataGridView2.MultiSelect = true;
 
-            // Setup ComboBoxes with users
+            dataGridView1.Click += DataGridView_Click;
+            dataGridView2.Click += DataGridView_Click;
+
             PopulateUserList();
 
-            // Event Handlers
             comboBox1.SelectedIndexChanged += ComboBox1_SelectedIndexChanged;
             comboBox2.SelectedIndexChanged += ComboBox2_SelectedIndexChanged;
             button1.Click += BtnCopyRight_Click;
             button2.Click += BtnCopyLeft_Click;
+            button3.Click += button3_Click;
+            button4.Click += Button4_Backup_Click;
         }
 
         private void PopulateUserList()
@@ -57,12 +63,12 @@ namespace _1C8_Base_Manager
 
             label1.Text = "Data loaded";
         }
-
         private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
             string selectedUser = comboBox1.SelectedItem.ToString();
             currentUserPath1 = $@"C:\Users\{selectedUser}\AppData\Roaming\1C\1CEStart\ibases.v8i";
             LoadConnectionsFromFile(currentUserPath1, dataGridView1);
+            label1.Text = "Data loaded..";
         }
 
         private void ComboBox2_SelectedIndexChanged(object sender, EventArgs e)
@@ -70,6 +76,7 @@ namespace _1C8_Base_Manager
             string selectedUser = comboBox2.SelectedItem.ToString();
             currentUserPath2 = $@"C:\Users\{selectedUser}\AppData\Roaming\1C\1CEStart\ibases.v8i";
             LoadConnectionsFromFile(currentUserPath2, dataGridView2);
+            label1.Text = "Data loaded...";
         }
 
         private void LoadConnectionsFromFile(string filePath, DataGridView gridView)
@@ -77,7 +84,6 @@ namespace _1C8_Base_Manager
             gridView.Rows.Clear();
             gridView.Columns.Clear();
 
-            // Add columns
             gridView.Columns.Add("Name", "Name");
             gridView.Columns.Add("Server", "Server");
             gridView.Columns.Add("Reference", "Reference");
@@ -135,7 +141,6 @@ namespace _1C8_Base_Manager
                     }
                 }
 
-                // Reload the destination file
                 LoadConnectionsFromFile(destFilePath, destGrid);
                 label1.Text = "Connections copied successfully!" ;
             }
@@ -164,7 +169,93 @@ App=Auto
 WA=1
 Version=8.3";
         }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (lastActiveGridView == null)
+            {
+                MessageBox.Show("Select table to delete.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
 
+            if (lastActiveGridView.SelectedRows.Count == 0)
+            {
+                return;
+            }
+
+            string currentUserPath = (lastActiveGridView == dataGridView1) ? currentUserPath1 : currentUserPath2;
+
+            DialogResult result = MessageBox.Show(
+                $"Are you sure you want to delete {lastActiveGridView.SelectedRows.Count} connections?",
+                "Confirm deletion",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                try
+                {
+                    var selectedRowsToDelete = lastActiveGridView.SelectedRows.Cast<DataGridViewRow>().ToList();
+
+                    string fileContent = File.ReadAllText(currentUserPath);
+                    foreach (DataGridViewRow row in selectedRowsToDelete.OrderByDescending(r => r.Index))
+                    {
+                        string connectionName = row.Cells[0].Value?.ToString();
+                        if (!string.IsNullOrEmpty(connectionName))
+                        {
+                            fileContent = RemoveConnectionBlock(fileContent, connectionName);
+                        }
+                    }
+                    File.WriteAllText(currentUserPath, fileContent);
+                    LoadConnectionsFromFile(currentUserPath, lastActiveGridView);
+                    lastActiveGridView.ClearSelection();
+                    MessageBox.Show("The selected connections have been removed successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error while deleting: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void DataGridView_Click(object sender, EventArgs e)
+        {
+            lastActiveGridView = (DataGridView)sender;
+            if (lastActiveGridView == dataGridView1)
+            {
+                dataGridView2.ClearSelection();
+            }
+            else
+            {
+                dataGridView1.ClearSelection();
+            }
+        }
+
+        private string RemoveConnectionBlock(string fileContent, string connectionName)
+        {
+            string pattern = $@"\[{Regex.Escape(connectionName)}\][^\[]*(?=\[|$)";
+            return Regex.Replace(fileContent, pattern, "", RegexOptions.Singleline);
+        }
+        private void Button4_Backup_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string currentUserPath = (lastActiveGridView == dataGridView1) ? currentUserPath1 : currentUserPath2;
+                if (string.IsNullOrEmpty(currentUserPath))
+                {
+                    MessageBox.Show("Select a user to create a backup.", "Attention", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+                string backupFilePath = currentUserPath + ".bak";
+
+                File.Copy(currentUserPath, backupFilePath, true);
+                MessageBox.Show($"Backup created: {backupFilePath}", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                label1.Text = "Создана резервная копия.";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error creating backup: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             string url = "https://github.com/e-gaydarzhi";
